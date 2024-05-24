1c52d8a91e2eeb4d4ce210f136354a35e7fb69e4
MichaelHinrichs <mchinrichs@gmail.com>
2024-05-24 15:37:15 -0500

    Use "path" string.

--- Program.cs
+++ Program.cs
@@ -11,8 +11,9 @@ namespace Tales_Runner_Extractor
             BinaryReader br = new(File.OpenRead(args[0]));
             br.BaseStream.Position = 32;
 
+            string path = Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]) + "//";
             int n = 0;
-            Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]));
+            Directory.CreateDirectory(path);
             while (br.BaseStream.Position < br.BaseStream.Length)
             {
                 br.ReadInt32();
@@ -21,16 +22,18 @@ namespace Tales_Runner_Extractor
                 br.ReadSingle();
                 int isCompressed = br.ReadInt32();
 
+
                 if (isCompressed == 1)
                 {
                     MemoryStream ms = new();
                     br.ReadInt16();
                     using (var ds = new DeflateStream(new MemoryStream(br.ReadBytes(sizeCompressed - 2)), CompressionMode.Decompress))
-                        ds.CopyTo(File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]) + "//" + n));
+                        ds.CopyTo(File.Create(path + n));
                     n++;
                     continue;
                 }
-                BinaryWriter bw = new(File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]) + "//" + n));
+
+                BinaryWriter bw = new(File.Create(path + n));
                 bw.Write(br.ReadBytes(sizeUncompressed));
                 bw.Close();
                 n++;
