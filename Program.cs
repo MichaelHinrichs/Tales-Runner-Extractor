addf1d9559595cbe67fd1c7a6dd160bf3b9722ff
MichaelHinrichs <mchinrichs@gmail.com>
2024-05-26 13:00:16 -0500

    Determine file extensions. I had to make some up.
    Closes #1.

--- Program.cs
+++ Program.cs
@@ -37,15 +37,58 @@ namespace Tales_Runner_Extractor
 
                 StreamReader sr = new(ms);
                 sr.BaseStream.Position = 0;
-                char[] magicChars = new char[4];
+                char[] magicChars = new char[18];
                 sr.ReadBlock(magicChars, 0, 4);
                 string magic = new(magicChars);
 
                 string extention = "";
-                switch(magic)
+                switch (magic)
                 {
                     case "DDS ":
                         extention = ".dds";
+                        break;
+                    case "OggS":
+                        extention = ".ogg";
+                        break;
+                    case "<?xm":
+                        extention = ".xml";
+                        break;
+                    case "TRGa":
+                        extention = ".trglf";//TRGameLevelFile
+                        break;
+                    case "Char":
+                        sr.ReadBlock(magicChars, 4, 5);
+                        magic = new string(magicChars).TrimEnd('\0');
+                        if (magic == "CharModel")
+                        {
+                            sr.ReadBlock(magicChars, 9, 5);
+                            magic = new string(magicChars).TrimEnd('\0');
+                            switch (magic)
+                            {
+                                case "CharModelPart2":
+                                    extention = ".cmp2";
+                                    break;
+                                case "CharModelMesh2":
+                                    extention = ".cmm2";
+                                    break;
+                                case "CharModelPart3":
+                                    extention = ".cmp3";
+                                    break;
+                                case "CharModelMesh3":
+                                    extention = ".cmm3";
+                                    break;
+                            }
+                        }
+                        else if (magic == "CharAnima")
+                        {
+                            sr.ReadBlock(magicChars, 9, 9);
+                            magic = new string(magicChars).TrimEnd('\0');
+
+                            if (magic == "CharAnimationFile2")
+                                extention = ".caf2";
+                            if (magic == "CharAnimationFile3")
+                                extention = ".caf3";
+                        }
                         break;
                 }
                 FileStream fs = File.Create(path + n + extention);
